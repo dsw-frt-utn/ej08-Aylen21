6ba91d4d2c48d3f783624c8a99eef8f0d81c2e7b
Aylen21 <aylenfajardo2022@gmail.com>
2026-05-04 01:49:51 -0300

    Problema 2 - Resuelto

---
+++ Dsw2026Ej8/Problema_2/Problema2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema_2
+{
+    internal class Problema2
+    {
+
+
+        public string CrearResumenVenta(long productCode,string productDescription,int quantity, decimal unitPrice) {
+
+              decimal total;
+
+            if (quantity <= 0)
+            {
+
+                total = 0;
+
+            }
+            else {
+
+                  total = quantity * unitPrice;
+            }
+            var claseAnonima = new { Code = productCode , Description = productDescription, Quantity = quantity, Total = total};
+
+            return $"{claseAnonima.Code} - {claseAnonima.Description} - {FormatearMoneda(claseAnonima.Total)} ";
+        
+        
+        
+        }
+
+
+        public string FormatearMoneda(decimal price)
+        {
+            return price.ToString("C");
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Dsw2026Ej8.Problema_1;
+using Dsw2026Ej8.Problema_2;
 
 namespace Dsw2026Ej8
 {
@@ -13,7 +14,16 @@ namespace Dsw2026Ej8
             string resultado = p.ObtenerEtiquetaProducto(12, "Mouse", 1400);
 
             Console.WriteLine(resultado);
-          
+            Console.WriteLine("     ----------  Problema 2 ----------       ");
+
+            Problema2 p2 = new Problema2();
+
+            Problema2 p23 = new Problema2();
+            string resultado2 = p2.CrearResumenVenta(123, "Veloz", -5, 100);
+            string resultado3 = p23.CrearResumenVenta(1234,"Ancho",4,100);
+
+            Console.WriteLine(resultado2);
+            Console.WriteLine(resultado3);
 
 
 
