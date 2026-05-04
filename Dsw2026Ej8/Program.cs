ae6f22b135b3853325cf5a6021cf6b39d3aa85e9
Aylen21 <aylenfajardo2022@gmail.com>
2026-05-04 01:13:52 -0300

    Problema 1 - Resuelto

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Dsw2026Ej8
+﻿using Dsw2026Ej8.Problema_1;
+
+namespace Dsw2026Ej8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("     ----------  Problema 1 ----------       " );
+
+            ProductHelper p = new ProductHelper();
+
+            string resultado = p.ObtenerEtiquetaProducto(12, "Mouse", 1400);
+
+            Console.WriteLine(resultado);
+          
+
+
+
         }
     }
 }
